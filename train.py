9173c706b0fc68b088053b4d0752a07e3120cba3
Omar Hamadou <hsouleyomar@gmail.com>
2025-11-07 10:42:01 +0100

    read data file

--- train.py
+++ train.py
@@ -1,3 +1,26 @@
+import csv
 import predict
 
-print(predict.prediction(200, 20, -0.05))
+dataset = []
+
+with open("data.csv", 'r') as file:
+	csv_reader = csv.reader(file)
+
+	next(csv_reader)
+	for line in csv_reader:
+		# print(line)
+		dataset.append(line)
+
+print(dataset)
+
+#Derivative
+#Prediction
+#Errors
+#Gradient
+#Gradient upate rule
+#cost function
+	#prediction
+	#errors
+	#squared errors
+	#sum
+	#cost/m
