ef6728b6c29e0cdaaa66a058fd2d4c586b4dd71b
Omar Hamadou <hsouleyomar@gmail.com>
2025-11-07 10:26:01 +0100

    add main function to predict.py

--- predict.py
+++ predict.py
@@ -2,3 +2,16 @@
 
 def prediction(mileage, teta0, teta1):
 	return teta0 + teta1 * mileage
+
+def main():
+	w = -0.05
+	b = 30
+
+	mileage = 200;
+
+	price = prediction(mileage, b, w)
+
+	print("predicted price: %d" % price)
+
+if __name__ == "__main__":
+	main()
---
+++ train.py
@@ -0,0 +1,3 @@
+import predict
+
+print(predict.prediction(200, 20, -0.05))
