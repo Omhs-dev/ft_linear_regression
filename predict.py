6a3aa09560434ba43388f9a10e9c4ea6d54bfb18
Omar Hamadou <hsouleyomar@gmail.com>
2025-11-11 10:29:59 +0100

    add values.json file to store theta0 and theta1 values

--- predict.py
+++ predict.py
@@ -1,7 +1,17 @@
 # linear regression
+import json
 
-def prediction(mileage, teta0, teta1):
-	return float(teta0) + float(teta1) * mileage
+try:
+	with open("values.json", "r") as file:
+		read_file = json.load(file)
+		theta0 = read_file.get("Theta0")
+		theta1 = read_file.get("Theta1")
+		print(f"Theta0: {theta0}, Theta1: {theta1}")
+except FileNotFoundError:
+	print("Error: file 'value.json' not found")
+
+def prediction(mileage, theta0, theta1):
+	return theta0 + theta1 * mileage
 
 def main():
 	w = -0.05
