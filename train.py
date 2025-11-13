87f65df2d370b774ae550cf640a96e4f0f9861e4
Omar Hamadou <hsouleyomar@gmail.com>
2025-11-13 11:01:55 +0100

    implementing cost function with extra helpers

--- train.py
+++ train.py
@@ -1,12 +1,11 @@
 import csv
-import pandas as pd
+# import pandas as pd
 import predict
 
-w_theta1 = 0
 b_theta0 = 0
+w_theta1 = 0
 x_mileage = 0
 y_price = 0
-learning_rate = 0.1
 dataset = []
 error_list= []
 
@@ -31,6 +30,7 @@ def load_data():
 		print("Error: file no found")
 		return None
 
+'''
 def normalize(data):
 	# value = [34, 2, 54, 63]
 
@@ -42,6 +42,7 @@ def normalize(data):
 x_data, y_data = load_data()
 
 normalize(x_data)
+'''
 #Derivative
 	#I already have the derivated of dJ/d_theta1 and dJ/d_theta0 based on the formula
 #Prediction
@@ -50,7 +51,7 @@ def error(w, b, x, y):
 # error = pred[i] - y[i]
 # pred = b + wx
 		return predict.prediction(x, w, b) - y
-# print("Error: %d" % error(0, 0, 2, 4))
+print("Error: %d" % error(0, 0, 1, 1))
 
 def calculate_errors(w, b, x_values, y_values):
 	errors = []
@@ -72,48 +73,66 @@ def w_theta1_gradient_errors(w, b, x_values, y_values):
 		errors.append(error(w, b, x_values[i], y_values[i]) * x_values[i])
 	return errors
 
-# print("errors sum: %f" % (sum(w_theta1_gradient_errors(0, 0, [1, 2, 3, 4], [1, 2, 2.5, 4])) / len([1, 2, 3, 4])))
+# print("errors: %s" % (w_theta1_gradient_errors(0, 0, [1, 2, 3, 4], [1, 2, 2.5, 4])))
 #Gradient
 def w_theta1_gradient(w, b, x_values, y_values):
 	return (sum(w_theta1_gradient_errors(w, b, x_values, y_values)) / len(x_values))
 
+# print("errors sum: %f" % (w_theta1_gradient(0, 0, [1, 2, 3, 4], [1, 2, 2.5, 4])))
+
 def b_theta0_gradient(w, b, x_values, y_values):
 	return (sum(b_theta0_gradient_errors(w, b, x_values, y_values)) / len(x_values))
 
-
 #Gradient upate rule
 def gradient_update_rule(x_values, y_values):
+	learning_rate = 0.1
+
 	global b_theta0
 	global w_theta1
-	gradient_update = []
 
 	prev_b_theta0 = b_theta0
 	prev_w_theta1 = w_theta1
-	
+	print("prev_b_theta0: %f" % prev_b_theta0)
+	print("prev_w_theta1: %f" % prev_w_theta1)
+
 	b_theta0 = b_theta0 - (learning_rate * (b_theta0_gradient(prev_w_theta1, b_theta0, x_values, y_values)))
 	w_theta1 = w_theta1 - (learning_rate * (w_theta1_gradient(w_theta1, prev_b_theta0, x_values, y_values)))
-	gradient_update.append(b_theta0)
-	gradient_update.append(w_theta1)
-	return gradient_update
-
-# def b_theta0_gradient_update(x_values, y_values):
-# 	global b_theta0
-# 	b_theta0 = b_theta0 - (learning_rate * (b_theta0_gradient(w_theta1, b_theta0, x_values, y_values)))
-# 	return b_theta0
-
-# print("update: %s" % (gradient_update_rule(x_mileage, y_price)))
-# print("update: %f" % (gradient_update_rule(x_mileage, y_price)))
-# print("update b: %s" % (b_theta0_gradient_update([1, 2, 3, 4], [1, 2, 2.5, 4])))
+	print("b_theta0: %f" % b_theta0)
+	print("w_theta1: %f" % w_theta1)	
+	return (b_theta0, w_theta1)
 
 x_mileage, y_price = load_data();
-gradient_update_rule(x_mileage, y_price)
-predicted_price = [predict.prediction(mileage, b_theta0, w_theta1) for mileage in x_mileage]
-print("b: %f" % b_theta0)
-print("w: %f" % w_theta1)
-print("price prediction: %s" % predict.prediction(x_mileage[0], b_theta0, w_theta1))
+new_theta0, new_theta1 = gradient_update_rule(x_mileage, y_price)
+# print("b: %f" % new_theta0)
+# print("w: %f" % new_theta1)
+# print("xmileage: %s" % x_mileage)
+# print("yprice: %s" % y_price)
+
+def squared_error(w, b, x_values, y_values):
+	res = []
+	for i in range(0, len(x_values)):
+		res.append(pow(error(w, b, x_values[i], y_values[i]), 2))
+	return res
+
+def sum_squarred_errors(w, b, x_values, y_values):
+	return sum(squared_error(w, b, x_values, y_values))
+
+print("errors sum: %s" % (squared_error(0, 0, [1, 2, 3, 4], [1, 2, 2.5, 4])))
+print("sum errors sum: %s" % (sum_squarred_errors(0, 0, [1, 2, 3, 4], [1, 2, 2.5, 4])))
+
 #cost function
+def cost_function(w, b, x_values, y_values):
+	cost = 0
+
+	cost = 1/(2 *len(x_values)) * sum_squarred_errors(w, b, x_values, y_values)
+	return cost
 	#prediction
-	#errors
-	#squared errors
-	#sum
-	#cost
+	
+# 	#errors
+# 	#squared errors
+# 	#sum
+# 	#cost
+
+# cost_function()
+
+print("cost: %s" % (cost_function(0, 0, [1, 2, 3, 4], [1, 2, 2.5, 4])))
